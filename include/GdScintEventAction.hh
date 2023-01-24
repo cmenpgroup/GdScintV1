ab7a31e9422015e3c106cd7dc51f6e6852ad7e31
jwgibbs <mikewood@jlab.org>
2023-01-24 09:20:19 -0500

    Removed the GdScintActionInitialization class

--- include/GdScintEventAction.hh
+++ include/GdScintEventAction.hh
@@ -1,29 +1,8 @@
 //
 /// \file GdScintEventAction.hh
 /// \brief Definition of the GdScintEventAction class
-#ifndef GdScintEventAction_h
-#define GdScintEventAction_h 1
-
-#include "G4UserEventAction.hh"
-#include "globals.hh"
-
-#include "G4SystemOfUnits.hh"
-#include "G4PhysicalConstants.hh"
-
-//2017: follwing have been changed:
-#ifdef ANALYSIS_USE
- #include "TROOT.h"
- #include "TFile.h"
- #include "TNtuple.h"
- #include "TTree.h"
- #include "TH1D.h"
-#endif
-
-class GdScintEventMessenger;
-
-class GdScintEventAction : public G4UserEventAction
-#ifndef GdScintEventAction_h
-#define GdScintEventAction_h 1
+#ifndef GdScintEventAction_H
+#define GdScintEventAction_H 1
 
 #include "G4UserEventAction.hh"
 #include "globals.hh"
@@ -78,34 +57,3 @@ class GdScintEventAction : public G4UserEventAction
 
 
 
-
-#ifndef GdScintEventAction_h
-#define GdScintEventAction_h 1
-
-#include "G4UserEventAction.hh"
-#include "globals.hh"
-
-class GdScintRunAction;
-
-/// Event action class
-///
-
-class GdScintEventAction : public G4UserEventAction
-{
-  public:
-    GdScintEventAction(GdScintRunAction* runAction);
-    virtual ~GdScintEventAction();
-
-    virtual void BeginOfEventAction(const G4Event* event);
-    virtual void EndOfEventAction(const G4Event* event);
-
-    void AddEdep(G4double edep) { fEdep += edep; }
-
-  private:
-    GdScintRunAction* fRunAction;
-    G4double     fEdep;
-};
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-#endif
